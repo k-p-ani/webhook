8e8aa080253b90baf75bda16c4285f30683a7f8f
ANI <ani@ANI-ka-MacBook-Air.local>
2021-02-15 19:42:40 +0530

    fourth commit

--- src/main/java/com/example/sslserver/SslServerApplication.java
+++ src/main/java/com/example/sslserver/SslServerApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import com.example.restservice.Greeting;
 
 @SpringBootApplication
 public class SslServerApplication {
@@ -27,5 +29,10 @@ class SecuredServerController {
 
 		System.out.println(" json request is \n" + jsonRequest + "\n");
 		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, produces = "application/json")
+	public Greeting greeting() {
+		return new Greeting("world ..!!");
 	}
 }
